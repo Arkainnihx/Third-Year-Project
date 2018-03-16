43b80172040513b763665d138f248009b8fa3967
William <william.jcs@gmail.com>
2018-03-16 22:19:45 +0000

    Basic map, navmesh, and agent testing.

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.Build;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour {
+
+	public Transform goal;
+	private NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(goal.position);
+	}
+	
+	void Update () {
+		if (agent.remainingDistance < 2) {
+			agent.ResetPath();
+		}
+	}
+}
