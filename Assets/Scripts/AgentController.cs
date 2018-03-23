cf1dffe8fad0e00a9c52af6453f2dd711e1c462f
William <william.jcs@gmail.com>
2018-03-23 00:14:50 +0000

    More agent stuff.

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -6,32 +6,49 @@ using UnityEngine.AI;
 
 public class AgentController : MonoBehaviour {
 
-	private Vector3 desiredPosition = new Vector3(0, 0, -20);
+	private Vector3 endPosition = new Vector3(0, 0, -20);
+	private Vector3 desiredPosition;
+	private Vector2 desiredVelocity = Vector2.zero;
+	private NavMeshAgent nav;
+	private Vector3[] path;
 	
 	void Start() {
-		NavMeshAgent nav = GetComponent<NavMeshAgent>();
-		NavMeshPath path = new NavMeshPath();
-		if (nav.CalculatePath(desiredPosition, path)) {
-			List<GameObject> pathPointList = new List<GameObject>();
-			foreach (Vector3 corner in path.corners) {
-				GameObject pathPoint = new GameObject();
-				pathPoint.GetComponent<Transform>().position = corner;
-				pathPointList.Add(pathPoint);
-			}
-		}
-		
+		nav = GetComponent<NavMeshAgent>();
+		GenerateInitialPath();
 	}
 	
 	void Update() {
 		
 	}
 
-	Vector3 CalculateMoveIncrement() {
-		
-		return Vector3.zero;
+	bool GenerateInitialPath() {
+		var newPath = new NavMeshPath();
+		if (nav.CalculatePath(endPosition, newPath)) {
+			path = newPath.corners;
+			var test = new List<Vector3>();
+			test.AddRange(path);
+		}
+		return path.Length > 0;
 	}
 
+	Vector2 CalculateMoveIncrement() {
+		var moveIncrement = Vector2.zero;
+		
+		return moveIncrement;
+	}
+	
 	void UpdateDesiredPosition() {
 		
 	}
+
+	bool isNewPositionDesired() {
+
+		return true;
+	}
+
+	Vector2 SocialForce() {
+		
+		return Vector2.zero;
+	}
+	
 }
